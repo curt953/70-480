0a08cbd8b9d8c5ab03b1463ed21b6fb7f7ce37a1
svile <svilen.nikolov1@abv.bg>
2018-12-13 16:08:25 +0200

    Commit on 13.12.2018. Added 3 new html files and 1 new js file.

---
+++ Exam Preparation/one.js
@@ -0,0 +1,16 @@
+function person(personName, personAge) {
+    return console.log(`The person's name is ${personName} and is ${personAge} years old.`);
+}
+
+let personOne = new person("Zak Nikolov", 24);
+
+async function asFunc() {
+    await person(personOne);
+
+    setTimeout(country => {
+        country = 'Bulgaria';
+        console.log(country);
+    }, 7000);
+}
+
+asFunc();
---
+++ README.md
@@ -0,0 +1,9 @@
+# curt953.github.io
+
+This repository contains the Soft Uni Web Fundamentals Project for a defense and also
+
+the Exam Preparation Projects for the 70-480 Programming in HTML5 with JavaScript and 
+
+CSS3 Microsoft Exam for MCP || MCSD (Microsoft Certified Professional; Microsoft 
+
+Certified Software Developer).
