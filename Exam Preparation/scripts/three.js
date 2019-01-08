419382f0aa1ad4288a66afa4b134ecbf4443afd8
zak_n <zak.n@processflows.co.uk>
2019-01-08 09:47:20 +0200

    Commit on 08.01.2019. Added the try...catch...finally implementation.

--- Exam Preparation/scripts/three.js
+++ Exam Preparation/scripts/three.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/* This block of code contains try...catch...finally implementation */
+/* This block of code contains try...catch...finally implementation
 
 window.onload = function () {
     try {
@@ -11,3 +11,142 @@ window.onload = function () {
     }
 };
 
+
+function WalkthroughArray() {
+    try {
+        let canvas = document.getElementById('myCanvas');
+        let context = canvas.getContext('2d');
+
+        context.fillStyle = '#000fff';
+        context.strokeStyle = 'red';
+
+        contxt.arc(50, 50, 25, 0, 360, false);
+        context.fill();
+        context.stroke();
+    } catch (e) {
+        if(e) {
+            console.error('Error name is: ', e.name, ', error type is: ', e.type);
+        }
+    } finally {
+        console.info('I am tired of your errors.');
+    }
+}
+
+window.onload = function () {
+      WalkthroughArray();
+};
+
+
+let canvas;
+let context;
+
+try {
+    document.getElementById('myCanvas');
+    context = canvas.getContext('2d');
+    contxt.arc(50, 50, 25, 0, 360);
+    context.fillStyle = 'blue';
+    context.strokeStyle = 'red';
+    context.fill();
+    context.stroke();
+}
+catch(e) {
+    context.strokeText(e.message, 50, 50);
+    console.error(e.message);
+}
+finally {
+    console.info('You motherfucker.');
+}
+
+window.onload = function () {
+
+    try {
+        WorkWithCanvas();
+    }
+    catch (e) {
+        console.error(e.message);
+    }
+
+    function WorkWithCanvas() {
+
+        let canvas = document.getElementById('myCanvas');
+        let context = canvas.getContext('2d');
+
+        contxt.arc(50, 50, 25, 0, 360, false);
+        context.fillStyle = 'blue';
+        context.strokeStyle = 'red';
+        context.fill();
+        context.stroke();
+
+    }
+
+};
+function WorkWithCanvas() {
+
+    try {
+        let canvas = document.getElementById('myCanvas');
+        let context = canvas.getContext('2d');
+
+        contxt.arc(50, 50, 25, 0, 360, false);
+        context.fillStyle = 'blue';
+        context.strokeStyle = 'red';
+        context.fill();
+        context.stroke();
+    }
+    catch (e) {
+        throw e;
+    }
+    finally {
+        console.info('Bring that ASS here.');
+    }
+
+}
+
+window.onload = function () {
+    WorkWithCanvas();
+};
+
+let ball = {
+    x: -1,
+    y: -1,
+    draw: function DrawBall() {
+        if(this.x < 0){
+            throw new Error('Invalid X coordinate');
+        }
+    }
+};
+
+window.onload = function () {
+    try {
+        let canvas = document.getElementById('myCanvas');
+        let context = canvas.getContext('2d');
+
+        ball.draw(context);
+    }
+    catch (e) {
+        console.error(e.message);
+    }
+};
+window.onload = function () {
+    try {
+        let a, b, c;
+
+        a = 5;
+        b = 10;
+
+        let result = multiplyNumbers(a, b, c);
+        console.log(result);
+    }
+    catch (e) {
+        console.error(e.message);
+    }
+
+    function multiplyNumbers(first, second, third) {
+
+        if (first === null || second === null || third === null) {
+            throw new Error('Forgot to initialize a number.');
+        }
+
+        return first * second * third;
+
+    }
+};*/
