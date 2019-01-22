26fd55cf9b62b915e9e0c4f485bd92b51c760e0a
curt953 <svilen.nikolov1@abv.bg>
2019-01-22 18:06:16 +0200

    Commit on 22.01.2019. Added the Practice Exam Answers and Explanations.xslx file and the referenced code in two.js. The .xslx file only contains 10/145 Q&A.

--- Exam Preparation/rerun/scripts/two.js
+++ Exam Preparation/rerun/scripts/two.js
@@ -1,2 +1,40 @@
-"use strict";
+// Code for 7th answer's correct and incorrect options:
 
+// Correct:
+let isValid = false;
+let elements = document.getElementsByTagName('input');
+for(let index = 0; index < elements.length; index++){
+    let element = elements[index];
+    if (element.type == "checkbox" && element.checked == true)  {
+        isValid = true;
+        break;
+    }
+}
+if(!isValid) {
+    window.alert("Please select a topping.");
+    return false;
+}
+
+// Incorrect:
+let isValid = false;
+let elements = document.getElementsByTagName('input');
+for(let index = 0; index < elements.length; index++){
+    let element = elements[index];
+    if (element.type == "checkbox" && element.checked == true)  {
+        isValid = true;
+        break;
+    }
+}
+if(!isValid) {
+    window.alert("Please select a topping.");
+}
+
+// Code for 8th answer's correct and incorrect options:
+
+// Correct:
+var regex = /[^\w\.@-]/;
+regex.test(data);
+
+// Incorrect:
+var regex = /[^\w\.@-]/;
+data.test(regex);
