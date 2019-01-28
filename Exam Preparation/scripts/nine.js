9615781f6dd83de960125e81a851073f7a22565b
zak_n <zak.n@processflows.co.uk>
2019-01-28 17:45:23 +0200

    Commit on 28.01.2019. Added new code for the rerun.

--- Exam Preparation/scripts/nine.js
+++ Exam Preparation/scripts/nine.js
@@ -1,2 +1,23 @@
 'use strict';
 
+function Person(first, last, age, gender, interests) {
+    this.name = {
+        first,
+        last
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+}
+
+Person.prototype.greeting = function () {
+    alert('Hi! I\'m ' + this.name.first + '.');
+};
+
+let personOne = new Person('Svilen', 'Nikolov', 24, 'M', 'Programming');
+// personOne.greeting();
+
+function Teacher(first, last, age, gender, interests, subject) {
+    Person.call(this, first, last, age, gender, interests);
+    this.subject = subject;
+}
